1ac427786d8935817e6b67eb1ad39f5effaef634
Dark-Fahim <itsmunna1066@gmail.com>
2025-08-11 21:50:47 +0600

    project setup

---
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -0,0 +1,11 @@
+
+
+const RightSideNav = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">right side Nav bar</h2>
+        </div>
+    );
+};
+
+export default RightSideNav;
